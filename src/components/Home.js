c90c5a82b93b060aa7c5a4c42e0d85125b0b879e
PopPaolo <paolo.pop@icloud.com>
2023-06-24 16:23:50 -0400

    Added picture and basic intro

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 function Home() {
     return <>
-        <h1>Home Page</h1>
+        <div className={'main-intro-container'}>
+            <img className={'main-intro-pic'} src={'media/Headshot_1.png'} alt={"Headshot 1"} height={200}/>
+            <p className={'main-intro'}><span id={'hello'}>Hello,</span> My name is Paolo Pop and I am a Software Engineering student at
+            <a className={'rit-link'} href={'https://www.rit.edu'}> Rochester institute of technology</a>. </p>
+        </div>
     </>
 }
 
