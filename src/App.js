8851b990f02f4a0a56edb1fc0d8c90b18e3265a4
PopPaolo <paolo.pop@icloud.com>
2023-06-24 15:24:44 -0400

    Separated NavBar into its own component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import Navigation from "./components/Navigation";
-import {Link, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Chat from "./components/Chat";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
     return (<>
-        <div className={'nav-bar'}>
-            <Link className={'nav-link'} to={'/'}><img src={'PP_1094x1177.png'} width={50} height={50} alt={"My icon"} className={"my-icon"}/></Link>
-            <Navigation/>
-        </div>
+        <NavBar/>
         <Routes>
             <Route path={""} element={<Home />}/>
             <Route path={"chat"} element={<Chat />} />
