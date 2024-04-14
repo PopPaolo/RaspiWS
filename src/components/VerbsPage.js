6ccf624a35db65ca08ff1b33657195efed692168
PopPaolo <130248987+PopPaolo@users.noreply.github.com>
2024-04-14 16:47:48 -0400

    Update VerbsPage.js
    Made card components responsively organized based on screen size

--- src/components/VerbsPage.js
+++ src/components/VerbsPage.js
@@ -1,24 +1,29 @@
 import React from "react";
-import Card from './Card.js';
-import Conjugator from './Conjugator/Conjugator.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from "./Card.js";
+import Conjugator from "./Conjugator/Conjugator.js";
 
-function VerbsPage (){
+const cardStyle = {
+    width: '350px',
+    height: '550px'
+};
+
+function VerbsPage() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-sm">
-                    <Card element={<Conjugator/>} cardTitle={"Coniuga Verbi:"}></Card>
+        <div className="container-sm d-flex justify-content-center" style={{paddingTop: "50px", paddingBottom: "50px"}}>
+            <div className="row justify-content-center">
+                <div className="col-12 col-sm-6 col-md-4 mb-4" style={cardStyle}>
+                    <Card element={<Conjugator/>} cardTitle={"Coniuga Verbi:"}/>
                 </div>
-                <div className="col-sm">
-                    <Card element={<Conjugator/>} cardTitle={"Test Card 2"}></Card>
+                <div className="col-12 col-sm-6 col-md-4 mb-4" style={cardStyle}>
+                    <Card element={<Conjugator/>} cardTitle={"Test Card 2"}/>
                 </div>
-                <div className="col-sm">
-                    <Card element={<Conjugator/>} cardTitle={"Test Card 3"}></Card>
+                <div className="col-12 col-sm-6 col-md-4 mb-4" style={cardStyle}>
+                    <Card element={<Conjugator/>} cardTitle={"Test Card 3"}/>
                 </div>
             </div>
         </div>
-    )
-};
+    );
+}
+
 
 export default VerbsPage;
