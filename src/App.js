aee472131922bc6cdd685b4128803f18fd81ca38
PopPaolo <130248987+PopPaolo@users.noreply.github.com>
2024-08-12 02:01:11 -0400

    Started viewport size tracking to be moved down into individual components for dynamic programming

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@
     @author Paolo Pop
 */
 
-
 //Style
 import "./App.css";
 
 // Functionality
 import { useMediaQuery, useTheme } from "@mui/material";
 import { vpSize } from "./vpSize";
-import{ useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // Page components
 import Home from "./components/Home";
@@ -18,40 +17,41 @@ import Sudoku from "./components/Sudoku/Sudoku";
 import Conjugator from "./components/Conjugator/Conjugator";
 
 function App() {
-  // Get device breakpoints
+  // Set up viewport size tracking
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down("md"));
-  const isMed = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const isMed = useMediaQuery(theme.breakpoints.between("md", "lg"));
 
   // State to track the viewport size
   const [viewportSize, setViewportSize] = useState(
-      isSmall ? vpSize.SMALL : (isMed ? vpSize.MEMDIUM : vpSize.LARGE)
+    isSmall ? vpSize.SMALL : isMed ? vpSize.MEDIUM : vpSize.LARGE,
   );
 
   // Effect to update the viewport size on resize
   useEffect(() => {
     const handleResize = () => {
-      let newSize = isSmall ? vpSize.SMALL : (isMed ? vpSize.MEMDIUM : vpSize.LARGE);
-      setViewportSize({
-        newSize,
-      });
+      setViewportSize(
+        isSmall ? vpSize.SMALL : isMed ? vpSize.MEDIUM : vpSize.LARGE,
+      );
     };
 
-    window.addEventListener('resize', handleResize);
+    handleResize(); // Call initially to set the correct size
+
+    window.addEventListener("resize", handleResize);
 
     // Cleanup event listener on component unmount
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [isSmall, isMed]);
 
   return (
     <>
       <div>
         <p>{`Viewport is ${viewportSize}`}</p>
       </div>
-       <Home />
-      {/*<Sudoku />*/}
+      {/*<Home />*/}
+      <Sudoku size={viewportSize} />
       {/*<Conjugator/>*/}
     </>
   );
