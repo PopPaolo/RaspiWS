3555520782e8b01c3ae3ed02e0aec59f2620175a
PopPaolo <paolo.pop@icloud.com>
2024-08-13 21:22:03 -0400

    Update Board.js
    New api logic

--- src/components/Sudoku/Board.js
+++ src/components/Sudoku/Board.js
@@ -9,20 +9,32 @@ const Board = ({ size, currentNumber, updateNumber }) => {
   const boardPositionClass = isSmall
     ? "position-absolute top-50 start-50 translate-middle"
     : isMid
-    ? "position-absolute top-50 start-50 translate-middle"
-    : "position-absolute top-50 translate-middle-y end-50";
+      ? "position-absolute top-50 start-50 translate-middle"
+      : "position-absolute top-50 translate-middle-y end-50";
 
   // Define the API endpoint
-  const apiUrl = "https://sudoku-api.vercel.app/api/dosuku";
+  // const apiUrl = "https://sudoku-api.vercel.app/api/dosuku";
+  const apiUrl = "https://www.youdosudoku.com/api/";
 
-  // Function to fetch a Sudoku puzzle
+  // Function to fetch a Sudoku puzzle 
   async function fetchSudoku() {
     try {
-      const response = await fetch(apiUrl);
+      const response = await fetch("https://youdosudoku.com/api/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          difficulty: "medium", // "easy", "medium", or "hard" (defaults to "easy")
+          solution: true, // true or false (defaults to true)
+          array: false // true or false (defaults to false)
+        })
+      })
       if (response.ok) {
         const data = await response.json();
-        const key = data.newboard.grids[0].value;
-        setSudokuGrid(key);
+        console.log(data)
+        // const key = data.newboard.grids[0].value;
+        // setSudokuGrid(key);
       } else {
         throw new Error("Network response was not ok");
       }
@@ -31,23 +43,25 @@ const Board = ({ size, currentNumber, updateNumber }) => {
     }
   }
 
+
   useEffect(() => {
     fetchSudoku();
   }, []);
 
   function updateCell(row, col) {
     if (sudokuGrid[row][col] === 0) {
+      // Update the selected cell with the currently selected number
       const newGrid = [...sudokuGrid];
-
       newGrid[row][col] = currentNumber;
-
       setSudokuGrid(newGrid);
+
+      updateNumber(0);
     }
   }
 
   return (
     <div className={boardPositionClass}>
-      {sudokuGrid.map((row, rowIndex) => (
+      {/* {sudokuGrid.map((row, rowIndex) => (
         <div key={rowIndex} className="d-flex">
           {row.map((num, cellIndex) => (
             <SmSquare
@@ -59,9 +73,10 @@ const Board = ({ size, currentNumber, updateNumber }) => {
             />
           ))}
         </div>
-      ))}
+      ))} */}
     </div>
   );
-};
+}
+
 
 export default Board;
