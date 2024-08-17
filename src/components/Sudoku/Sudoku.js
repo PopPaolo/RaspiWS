b901317d5bb37fce887c9acdfd3d138f2dda9c76
PopPaolo <paolo.pop@icloud.com>
2024-08-17 00:42:45 -0400

    Update Sudoku.js
    Readded tracking breakpoint tracking for dynamic page reordering.
    Updated initial state of the currently selected number on the number legend.

--- src/components/Sudoku/Sudoku.js
+++ src/components/Sudoku/Sudoku.js
@@ -4,8 +4,13 @@ import Board from "./Board";
 import NumberLegend from "./NumberLegend";
 
 function Sudoku({ size }) {
-  const [currentNumber, setCurrentNumber] = useState(0);
-  const [selectedCell, setSelectedCell] = useState([])
+  const [currentNumber, setCurrentNumber] = useState("0");
+  // TODO
+  // To implement for the selection of a cell and then a number to fill the cell
+  const [selectedCell, setSelectedCell] = useState([]);
+
+  const isSmall = size === "small";
+  const isMid = size === "medium";
 
   return (
     <>
