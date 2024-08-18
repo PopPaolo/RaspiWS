62bccebbc5fa84313f66af0f34fb79e390a5b73d
PopPaolo <130248987+PopPaolo@users.noreply.github.com>
2024-08-17 23:45:16 -0400

    Update NumberLegend.js
    Added mapping to the legend buttons.
    
    Improved number update logic.
    
    Added functionality for mapping entered symbols to a number in order to update the main sudoku grid.

--- src/components/Sudoku/NumberLegend.js
+++ src/components/Sudoku/NumberLegend.js
@@ -4,120 +4,130 @@
     @author Paolo Pop
 */
 
-
 // ######################## FUNCTIONALITY ########################
 // #
-import React, { useState } from "react";
+import React, { useState } from "react"
 // #
 // ###############################################################
 
+function NumberLegend({ size, updateNumber, editSymbols, symbols, setSymbols }) {
+  const [selectedIndex, setSelectedIndex] = useState([])
+  // Move symbols up to Sudoku.js level
 
-function NumberLegend({ size, updateNumber }) {
-    const [selectedNum, setSelectedNum] = useState("0")
-    // Move symbols up to Sudoku.js level
-    const [symbols, setSymbols] = useState(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-    const isSmall = size === "small";
-    const isMid = size === "medium";
+  const isSmall = size === "small"
+  const isMid = size === "medium"
 
-    const fontSize = isSmall ? "6vw" : isMid ? "5vw" : "7vh";
-    const boxSide = isSmall ? "9vw" : isMid ? "7vw" : "8vh";
+  const fontSize = isSmall ? "6vw" : isMid ? "5vw" : "7vh"
+  const boxSide = isSmall ? "13vw" : isMid ? "7vw" : "8vh"
 
-    const NumBlock = ({ num }) => {
-        // The block will have a light blue background if it matches the currently selected number
-        const isSelected = num === selectedNum;
+  function NumBlock({ num, rowIndex, colIndex }) {
+    const isSelected = JSON.stringify(selectedIndex) === JSON.stringify([rowIndex, colIndex])
+    // The block will have a light blue background if it matches the currently selected number
 
-        return (
-            <button
-                type="button"
-                className={"d-flex justify-content-center align-items-center " +
-                    " border-2 border-dark rounded-3"
-                }
-                onClick={() => {
-                    updateNumber(num)
-                    setSelectedNum(num)
-                }}
-                style={{
-                    width: boxSide,
-                    height: boxSide,
-                    fontSize: fontSize,
-                    backgroundColor: isSelected ? "lightblue" : "whitesmoke",
-                    border: "solid",
-                }}
-            >
-                {num}
-            </button>
-        )
-    }
-
-    const SymbolBlock = ({ symbol, index }) => {
-        return (
-            <input
-                key={index}
-                type="text"
-                className={"d-flex justify-content-center align-items-center " +
-                    " border-2 border-dark rounded-3"}
-                style={{
-                    width: boxSide,
-                    height: boxSide,
-                    fontSize: fontSize,
-                    border: "solid",
-                }}
-                placeholder={symbol}
-                onBlur={(e) => { updateSymbol(e.target.value, index) }}
-            />
-        );
-    }
-
-    function updateSymbol(newSymbol, index) {
-        if (newSymbol !== "") {
-            let newSymbols = [...symbols];
-            newSymbols[index] = newSymbol;
-
-            setSymbols(newSymbols);
+    function getNum(symbol) {
+      // Convert a given symbol to it's numerical equivalent on a 3x3 array
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (symbols[i][j] === symbol) {
+            return i * 3 + (j + 1)
+          }
         }
+      }
+      return null
     }
 
     return (
-        <div
-            className={"d-flex flex-column flex-lg-row gap-4"}
-        >
-            <div
-                className={"d-flex flex-row flex-lg-column gap-1"}
-            >
-                {/* {symbols.map(
-                    (symbol, symbolIndex) => {
-                        console.log(symbol);
-                        <NumBlock num={symbol} key={symbolIndex} />
-                    })
-                } */}
-                <NumBlock num={symbols[0]} index={0} />
-                <NumBlock num={symbols[1]} index={1} />
-                <NumBlock num={symbols[2]} index={2} />
-                <NumBlock num={symbols[3]} index={3} />
-                <NumBlock num={symbols[4]} index={4} />
-                <NumBlock num={symbols[5]} index={5} />
-                <NumBlock num={symbols[6]} index={6} />
-                <NumBlock num={symbols[7]} index={7} />
-                <NumBlock num={symbols[8]} index={8} />
-            </div>
-            <div
-                className={"d-flex flex-row flex-lg-column gap-1"}
-            >
-                {symbols.map((symbol, symbolIndex) => {
-                    <SymbolBlock symbol={symbol} index={symbolIndex} />
-                })}
-                <SymbolBlock symbol={1} index={0} />
-                <SymbolBlock symbol={2} index={1} />
-                <SymbolBlock symbol={3} index={2} />
-                <SymbolBlock symbol={4} index={3} />
-                <SymbolBlock symbol={5} index={4} />
-                <SymbolBlock symbol={6} index={5} />
-                <SymbolBlock symbol={7} index={6} />
-                <SymbolBlock symbol={8} index={7} />
-                <SymbolBlock symbol={9} index={8} />
-            </div>
-        </div>
+      <button
+        type="button"
+        className={"d-flex justify-content-center align-items-center border-3 border-dark rounded-3 m-1"}
+        onClick={() => {
+          updateNumber(getNum(num))
+          setSelectedIndex([rowIndex, colIndex])
+        }}
+        style={{
+          width: boxSide,
+          height: boxSide,
+          fontSize: fontSize,
+          backgroundColor: isSelected ? "lightblue" : "whitesmoke",
+          border: "solid",
+        }}
+      >
+        {num}
+      </button>
     )
+  }
+
+  const EditBlock = ({ num, rowIndex, colIndex }) => {
+    return (
+      <input
+        type="text"
+        className={"d-flex justify-content-center align-items-center text-center border-3 border-dark rounded-3 m-1"}
+        style={{
+          width: boxSide,
+          height: boxSide,
+          fontSize: fontSize,
+          border: "solid",
+        }}
+        placeholder={num}
+        onBlur={(e) => {
+          // TODO
+          //  Change to only allow one character long inputs
+          //  and give and error message about it.
+
+          updateSymbol(e.target.value, rowIndex, colIndex)
+        }}
+      />
+    )
+  }
+
+  function updateSymbol(newSymbol, rowIndex, colIndex) {
+    if (newSymbol !== "") {
+      let newSymbols = [...symbols]
+      newSymbols[rowIndex][colIndex] = newSymbol
+
+      setSymbols(newSymbols)
+    }
+  }
+
+  return (
+    <div>
+      {editSymbols ? (
+        <div className={"d-flex flex-column"}>
+          {symbols.map((row, rowIndex) => (
+            <div key={rowIndex} className={"d-flex flex-row"}>
+              {row.map((symbol, symbolIndex) => {
+                return (
+                  <EditBlock
+                    key={`${rowIndex}-${symbolIndex}`}
+                    num={symbol}
+                    rowIndex={rowIndex}
+                    colIndex={symbolIndex}
+                  />
+                )
+              })}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={"d-flex flex-column"}>
+          {symbols.map((row, rowIndex) => (
+            <div key={rowIndex} className={"d-flex flex-row"}>
+              {row.map((symbol, symbolIndex) => {
+                return (
+                  <NumBlock
+                    key={`${rowIndex}-${symbolIndex}`}
+                    num={symbol}
+                    rowIndex={rowIndex}
+                    colIndex={symbolIndex}
+                  />
+                )
+              })}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default NumberLegend;
+export default NumberLegend
