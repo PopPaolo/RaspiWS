82c2f2435326192122ebb5bc5b1c9208b7f7e8ef
PopPaolo <130248987+PopPaolo@users.noreply.github.com>
2024-08-17 23:47:43 -0400

    Update SmSquare.js
    Changed cell style for game board

--- src/components/Sudoku/SmSquare.js
+++ src/components/Sudoku/SmSquare.js
@@ -3,23 +3,23 @@
     for the cells that make up the board in Board.js.
     @author Paolo Pop
 */
-import React from "react";
+import React from "react"
 
 const SmSquare = ({ size, num, rowIndex, colIndex, updateCell }) => {
-  const isSmall = size === "small";
-  const isMid = size === "medium";
+  const isSmall = size === "small"
+  const isMid = size === "medium"
 
-  const numFontSize = isSmall ? "6vw" : isMid ? "5vw" : "7vh";
-  const boxSide = isSmall ? "8vw" : isMid ? "7vw" : "8vh";
+  const numFontSize = isSmall ? "7vw" : isMid ? "5vw" : "7vh"
+  const boxSide = isSmall ? "10vw" : isMid ? "7vw" : "8vh"
 
   // Function to determine the border style
   const getBorderStyle = () => {
-    let classes = "";
-    if (rowIndex === 2 || rowIndex === 5) classes += ` border-3 border-end border-black`;
-    if (colIndex === 2 || colIndex === 5) classes += ` border-3 border-bottom border-black`;
+    let classes = ""
+    if (rowIndex === 2 || rowIndex === 5) classes += ` border-3 border-end border-black`
+    if (colIndex === 2 || colIndex === 5) classes += ` border-3 border-bottom border-black`
 
-    return classes;
-  };
+    return classes
+  }
 
   return (
     <div className={getBorderStyle()}>
@@ -27,18 +27,18 @@ const SmSquare = ({ size, num, rowIndex, colIndex, updateCell }) => {
         key={colIndex}
         type="button"
         onClick={() => updateCell(rowIndex, colIndex)}
-        className="d-flex justify-content-center align-items-center rounded-2 border-2 border-dark-subtle"
+        className="d-flex justify-content-center align-items-center rounded-2 border-1 border-dark-subtle fw-medium"
         style={{
           width: boxSide,
           height: boxSide,
           fontSize: numFontSize,
-          borderStyle: "solid"
+          borderStyle: "solid",
         }}
       >
         {num}
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default SmSquare;
+export default SmSquare
